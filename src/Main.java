8b08deed028d5f7c691b0300d61243311b258f62
Peiyu Huang <w0472206@nscc.ca>
2024-01-31 08:08:00 -0400

    for loop for user input team name

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Fantasy Hokey Application");
-        System.out.println("Team Entry:");
+        System.out.println("Team Entry");
         System.out.println("==========================");
 
-        Scanner teamOne = new Scanner(System.in);  // Create team1 name
-        System.out.println("Enter name for team # 1:");
 
-        String teamOneName = teamOne.nextLine();  // Read user input
-        System.out.println(teamOneName);  // Output user input
+        for (int i =1; i <=3; i++ )
+        {
+            Scanner teamOne = new Scanner(System.in);  // Create team1 name
+            System.out.println("Enter name for team # " + i + ": ");
+            String teamOneName = teamOne.nextLine();  // Read user input
 
+            if (teamOneName.length() < 3)
+            {
+                i--;  // make it back to i-1, will run in same loop again
+            }
+            else
+            {
+                System.out.println(teamOneName);  // Output user input
+            }
+        }
 
+    } // public static void main
 
-
-    }
-}
+} //public class Main
